456f88d7445039cca46ff35bc624541e33e4136f
ShreyasNehe <shreyas.nehe22@gmail.com>
2023-12-13 15:26:29 +0530

    changed file name to "index.php"

--- add_new_row.php
+++ add_new_row.php
@@ -77,7 +77,7 @@
 
             <div>
                <button type="submit" class="btn btn-success" name="submit">Save</button>
-               <a href="index2.php" class="btn btn-danger">Cancel</a>
+               <a href="index.php" class="btn btn-danger">Cancel</a>
             </div>
          </form>
       </div>
